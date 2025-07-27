d9e636f76f868e517d89ba061caabebefebd7095
Alexandr Siryj <theobroma222@gmail.com>
2025-07-27 22:58:41 +0300

    eslint-plugin-react

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
 import theobromaConfig from '@theobroma222/eslint-config-myconfig';
 
 export default tseslint.config(
@@ -14,19 +15,34 @@ export default tseslint.config(
             theobromaConfig,
         ],
         files: ['**/*.{ts,tsx}'],
+        ...reactPlugin.configs.flat.recommended,
         languageOptions: {
             ecmaVersion: 2020,
             globals: globals.browser,
+            ...reactPlugin.configs.flat.recommended.languageOptions,
         },
         plugins: {
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
+            react: reactPlugin,
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
         },
         rules: {
             ...reactHooks.configs.recommended.rules,
             'react-refresh/only-export-components': [
                 'warn',
                 { allowConstantExport: true },
+            ],
+            'react/function-component-definition': [
+                2,
+                {
+                    namedComponents: 'arrow-function',
+                    unnamedComponents: 'arrow-function',
+                },
             ],
         },
     }
